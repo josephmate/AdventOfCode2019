8b8f272999eeae0ad123ad4f861afda06875fd2d
Joseph Mate <mate.joseph@gmail.com>
2019-12-01 11:11:15 -0500

    completed part 1

--- 01/The_Tyranny_of_the_Rocket_Equation.py
+++ 01/The_Tyranny_of_the_Rocket_Equation.py
@@ -2,9 +2,11 @@ import sys
 from functional import seq # from PyFunctional
 
 lines = sys.stdin.readlines()
-result = (seq(lines)
-                .map(lambda line: line.rstrip())
-                .map(lambda line: int(line))
+preprocessed = (seq(lines)
+                  .map(lambda line: line.rstrip())
+                  .map(lambda line: int(line))
+                )
+fuelForModules = (preprocessed
                 # Fuel required to launch a given module is based on its mass.
                 # Specifically, to find the fuel required for a module,
                 # take its mass,
@@ -14,6 +16,24 @@ result = (seq(lines)
                 .map(lambda mass : (mass//3) - 2)
              ).sum()
 
-print(str(result) + "\n")
+print("Part 1: " + str(fuelForModules) + "\n")
+
+def addOnFuelForFuel(moduleWeight):
+    uncoveredFuelMass = moduleWeight
+    fuelForFuel = 0
+    while uncoveredFuelMass > 0:
+        uncoveredFuelMass = (uncoveredFuelMass//3) - 2
+        if uncoveredFuelMass > 0:
+            fuelForFuel = fuelForFuel + uncoveredFuelMass
+    return moduleWeight + fuelForFuel
 
 
+fuelForModulesAndFuel = (preprocessed
+                  .map(lambda mass : (mass//3) - 2)
+                  # Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2.
+                  # However, that fuel also requires fuel, and that fuel requires fuel, and so on.
+                  # Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass,
+                  # if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+                  .map(addOnFuelForFuel)
+                  ).sum()
+print("Part 2: " + str(fuelForModulesAndFuel) + "\n")
