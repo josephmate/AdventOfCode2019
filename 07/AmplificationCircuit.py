0ef52a8022ffdb1f10503e688838136869d3e153
Joseph Mate <mate.joseph@gmail.com>
2019-12-12 21:47:40 -0500

    getting closer to the solution. understand problem after commenting test case
    1) need to remember memory
    2) need to remember the position we stopped when we output to next phase
    3) need to pass 2 values the first time (phase, input)
    4) need to pas 1 value all other times

--- 07/AmplificationCircuit.py
+++ 07/AmplificationCircuit.py
@@ -29,8 +29,8 @@ def getArgValue(memory, argMode, memoryPosition):
         raise Exception("getArgValue: unexpected argMode " + str(argMode) + " " + str(memoryPosition))
 
 
-def runProgram(memory, inputFromUser):
-    currentPosition = 0
+def runProgram(memory, inputFromUser, startingPosition=0):
+    currentPosition = startingPosition
     inputPosition = 0
     while True:
         if currentPosition >= len(memory):
@@ -79,8 +79,8 @@ def runProgram(memory, inputFromUser):
         # print
         elif currentOpCode == 4:
             firstArgVal = getArgValue(memory, firstArgMode, currentPosition + 1)
-            return firstArgVal
             currentPosition = currentPosition + 2
+            return firstArgVal, currentPosition
 
         # jump if
         elif currentOpCode == 5 or currentOpCode == 6:
@@ -113,7 +113,7 @@ def runProgram(memory, inputFromUser):
         else:
             raise Exception(str(currentOpCode) + " is not recognized")
 
-    return memory
+    return "DONE"
 
 
 def parseMemoryFromStr(programStr):
@@ -156,6 +156,34 @@ def solve(programString):
 
     return maxSoFar
 
+
+def runUntilCompletion(permutation, originalMemory):
+    phaseMemory = []
+    phaseMemory = []
+    for i in range(0, 5):
+        phaseMemory.append(originalMemory.copy())
+
+    while True:
+        for idx in range(0, 5):
+            phaseSetting = permutation[idx]
+            currentResult = runProgram(originalMemory.copy(), [phaseSetting, previousResult])
+            if currentResult == "DONE":
+                return previousResult
+            previousResult = currentResult
+
+
+def solvePart2(programString):
+    originalMemory = parseMemoryFromStr(programString)
+    allPhaseSettingSequences = permutations([5, 6, 7, 8, 9])
+    maxSoFar = 0
+    for permutation in allPhaseSettingSequences:
+        outputSignal = runUntilCompletion(permutation, originalMemory)
+        if outputSignal > maxSoFar:
+            maxSoFar = outputSignal
+
+    return maxSoFar
+
+
 try:
     print("should print 43210")
     print(str(solve("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")))
@@ -163,6 +191,39 @@ try:
     print(str(solve("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")))
     print("should print 65210")
     print(str(solve("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")))
+
+    print("Part 2 tests")
+    print("should print 139629729")
+    #  0  3,26,               read and save to addr 26
+    #  2  1001,26,-4,26,      add addr 26 with -4 and save in addr 26
+    #  6  3,27,               read and save to addr 27
+    #  8  1002,27,2,27,       multiply addr 27 with 2 and save in addr 27
+    # 12  1,27,26,27,         add addr 27 addr 26 and save to addr 27
+    # 16  4,27,               output addr 27
+    # 18  1001,28,-1,28,      add addr 28 and value -1 and save to addr 28
+    # 22  1005,28,6,          jump if addr 28 is not 0, jump to 6
+    # 25  99,
+    # 26  0,0,5
+    print(str(solve("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5")))
+    print("should print 18216")
+    #  0  3,52,
+    #  2  1001,52,-5,52,
+    #  6  3,53,
+    #  8  1,52,56,54,
+    # 12  1007,54,5,55,
+    # 16  1005,55,26,
+    # 20  1001,54,-5,54,
+    # 24  1105,1,12,
+    # 27  1,53,54,53,
+    # 31  1008,54,0,55,
+    # 35  1001,55,1,55,
+    # 39  2,53,55,53,
+    # 43  4,53,
+    # 45  1001,56,-1,56,
+    # 49  1005,56,6,
+    # 52  99,
+    # 53  0,0,0,0,10
+    print(str(solve("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10")))
 except Exception as e:
     print(str(e))
 
@@ -170,3 +231,5 @@ with open('input.txt', 'r') as fp:
     line = fp.readline()
     print("Part 1")
     print(str(solve(line)))
+    print("Part 2")
+    print(str(solvePart2(line)))
